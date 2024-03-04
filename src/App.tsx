ae38a675e0270c68a97a0884a0eafc84b7e063e4
Zhaoyu Qu <113986077+Zhaoyu-Qu@users.noreply.github.com>
2024-03-05 12:27:31 +1300

    apply wrapper context providers

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './AuthContext';
+import { ErrorProvider } from './ErrorContext';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
       <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
@@ -17,6 +21,15 @@ function App() {
             </Typography>
           </Toolbar>
         </AppBar>
+        <Container component="main" sx={{ mt: 2, mb: 2, flex: 1 }}>
+          <ErrorProvider>
+            <AuthProvider>
+              <QueryClientProvider client={queryClient}>
+                <></>
+              </QueryClientProvider>
+            </AuthProvider>
+          </ErrorProvider>
+        </Container>
         <Box component="footer" sx={{ py: 3, px: 2, mt: 'auto', backgroundColor: (theme) => theme.palette.grey[200] }}>
           <Container maxWidth="sm">
             <Typography variant="body1">
